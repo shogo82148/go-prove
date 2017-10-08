0a0ed6cf020c1993f4f94c9fab2fa8ccf99ec577
Ichinose Shogo <shogo82148@gmail.com>
2017-10-08 20:56:56 +0900

    update Golang to 1.9.1

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -3,4 +3,4 @@
 CURRENT=$(cd "$(dirname "$0")" && pwd)
 docker run --rm -it \
     -v "$CURRENT":/go/src/github.com/shogo82148/go-prove \
-    -w /go/src/github.com/shogo82148/go-prove golang:1.9 "$@"
+    -w /go/src/github.com/shogo82148/go-prove golang:1.9.1 "$@"
