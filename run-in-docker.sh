35f33077f6ccc41e3a6af567898201c5177c24ab
Ichinose Shogo <shogo82148@gmail.com>
2020-01-18 19:14:40 +0900

    update Go 1.13.6

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -6,4 +6,4 @@ docker run --rm -it \
     -e "GOOS=${GOOS:-linux}" -e "GOARCH=${GOARCH:-amd64}" -e "CGO_ENABLED=${CGO_ENABLED:-0}" \
     -v "$CURRENT":/go/src/github.com/shogo82148/go-prove \
     -v "$CURRENT/.mod":/go/pkg/mod \
-    -w /go/src/github.com/shogo82148/go-prove golang:1.12.4 "$@"
+    -w /go/src/github.com/shogo82148/go-prove golang:1.13.6 "$@"
