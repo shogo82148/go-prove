cc3786b4e9dae7efad1f81b5b235f18ecadfc1d2
Ichinose Shogo <shogo82148@gmail.com>
2020-08-12 12:33:38 +0900

    build with Go 1.15

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -6,4 +6,4 @@ docker run --rm -it \
     -e "GOOS=${GOOS:-linux}" -e "GOARCH=${GOARCH:-amd64}" -e "CGO_ENABLED=${CGO_ENABLED:-0}" \
     -v "$CURRENT":/go/src/github.com/shogo82148/go-prove \
     -v "$CURRENT/.mod":/go/pkg/mod \
-    -w /go/src/github.com/shogo82148/go-prove golang:1.14.0 "$@"
+    -w /go/src/github.com/shogo82148/go-prove golang:1.15.0 "$@"
